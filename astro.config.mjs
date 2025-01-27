8bd0597bbc60df7d23bcf79c3be6cf2e36ba8f24
satoru <satoru4274@gmail.com>
2025-01-27 23:36:53 +0900

    ADDED:remove localfont

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config'
-import localIntegration from './src/integrations/font-optimizer';
 
 export default defineConfig({
   integrations: [
-    localIntegration()
   ]
 })
