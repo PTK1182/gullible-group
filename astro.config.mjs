8db41d634f640d30d5ddab0827ca7a67d69c50e8
satoru <satoru4274@gmail.com>
2025-01-27 20:57:41 +0900

    REMOVE:css plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
-import localIntegration from './src/integrations/font-optimizer';
-import compress from 'astro-compress';
 
 export default defineConfig({
-  integrations: [compress(),localIntegration()]
+  integrations: [
+  ]
 })
