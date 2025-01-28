63d4a38ad14426ed0313d64423a6db0577270508
satoru <satoru4274@gmail.com>
2025-01-28 20:48:29 +0900

    ADDED:responsive width

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {colors:{
+			primary: {
+				'50': '#fffaeb',
+				'100': '#fdf1c8',
+				'200': '#fbe08c',
+				'300': '#f9ca50',
+				'400': '#f8b62d',
+				'500': '#f2920e',
+				'600': '#d66e09',
+				'700': '#b14b0c',
+				'800': '#903a10',
+				'900': '#763011',
+				'950': '#441704'
+			},
+		}},
 	},
 	plugins: [],
 }
